679e1405023ff31fbf8f2f40af014f0df621f91f
Nikolay Raykov <nraykov@Nikolays-MacBook-Air.local>
2022-03-04 21:37:34 +0200

    revert change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
-}).catch(error => {
-    console.log('Error establishing MongoDB connection. Error:', error.message);
-});   
+mongoose.connect(process.env.MONGODB_URL)
