87ae61422771fbda58a69df38c17475e58b34f0c
Nikolay Raykov <nraykov@Nikolays-MacBook-Air.local>
2022-03-04 20:45:28 +0200

    add error msg

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
+}).catch(error => {
+    console.log('Error establishing MongoDB connection. Error:', error.message);
+});   
